943b4c23ac092a8a2dbefa3b6a2aee28bfe1e26b
c_hwanch <c_hwanch@qti.qualcomm.com>
2019-06-25 13:50:44 +0900

    update

--- grok_info_server/rundbserver.sh
+++ grok_info_server/rundbserver.sh
@@ -1,5 +1,6 @@
 BASEDIR=$(dirname $0)
 CERTDIR='/var/lib/tomcat7/webapps/source/https'
-source ${BASEDIR}/venv/devenv/bin/activate
+source ${BASEDIR}/venv/bin/activate
 cd ${BASEDIR}/opengrok_api
 gunicorn --certfile=${CERTDIR}/certificate.pem --keyfile=${CERTDIR}/https.key --bind 0.0.0.0:9999 opengrok_api.wsgi --daemon --reload
+#gunicorn --bind 0.0.0.0:9999 opengrok_api.wsgi --daemon --reload
